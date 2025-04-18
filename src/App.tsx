6dc8f0257d7f7cf4285d7a970d7f839666c7c076
zobla <zobla.kv@gmail.com>
2025-04-18 22:09:45 +0200

    add page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className='text-center text-3xl font-bold mt-5 tracking-[0.25rem]'>
+        Doctor appointment
+      </h1>
+      <section>
+        <h2 className='text-center'>Doctors</h2>
+      </section>
+      <section>
+        <h2 className='text-center'>Booked appointments</h2>
+      </section>
+    </div>
+  );
 }
 
 export default App;
