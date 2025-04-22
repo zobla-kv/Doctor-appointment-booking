52a4bfe4a204b1fa82b630896c76d2c79cade1db
zobla <zobla.kv@gmail.com>
2025-04-22 12:17:09 +0200

    change how ref is passed to Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,24 +1,26 @@
-import { ForwardedRef, memo } from 'react';
+import React from 'react';
+import { memo } from 'react';
 
 interface ButtonProps {
   text: string;
   styles?: string;
-  ref?: ForwardedRef<HTMLButtonElement>;
   onClick: () => void;
 }
 
-function Button({ text, styles, ref, onClick, ...props }: ButtonProps) {
-  return (
-    <button
-      {...props}
-      ref={ref}
-      role='button'
-      className={'btn-primary focus-outline w-[100px] ' + styles}
-      onClick={onClick}
-    >
-      {text}
-    </button>
-  );
-}
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ text, onClick, styles, ...props }, ref) => {
+    return (
+      <button
+        {...props}
+        ref={ref}
+        role='button'
+        className={'btn-primary focus-outline w-[100px] ' + styles}
+        onClick={onClick}
+      >
+        {text}
+      </button>
+    );
+  }
+);
 
 export default memo(Button);
